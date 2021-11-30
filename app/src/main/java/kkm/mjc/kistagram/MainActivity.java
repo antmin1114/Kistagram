333c0ed54e1a9a031dbaaaf05e97f70cd1faba6c
Kimin <antmin1114@naver.com>
2021-11-30 15:46:42 +0900

    Initial commit

---
+++ app/src/main/java/kkm/mjc/kistagram/MainActivity.java
@@ -0,0 +1,14 @@
+package kkm.mjc.kistagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
